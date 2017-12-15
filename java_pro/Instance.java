b776965e4d9287a1e1c15d3be85799cea9ea2c89
Devisrios <33158227+Devisrios@users.noreply.github.com>
2017-12-15 12:24:23 +0530

    Create Instance.java

---
+++ java_pro/Instance.java
@@ -0,0 +1,14 @@
+//create an object of your own string class and compare against the java string class
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s = "abc";
+if (s instanceof java.lang.Boolean) {
+System.out.println("yes");
+}
+	}
+}
